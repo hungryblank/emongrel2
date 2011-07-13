037834c4da770cacf9e38956bcaf0fdb080d3c00
hungryblank <hungryblank@gmail.com>
2011-07-13 15:03:36 -0700

    include example receiver

--- examples/em2_receiver.erl
+++ examples/em2_receiver.erl
@@ -4,28 +4,13 @@
 %% API
 %%
 
--export([socket/1,
-         run/2]).
+-export([loop/1]).
 
-%% @doc initialize a socket for the receiver
--spec socket(Args::list()) -> {ok, Socket::erlzmq:erlzmq_socket()}.
-socket(Args) ->
-    Address = proplists:get_value(address, Args),
-    Identity = proplists:get_value(identity, Args),
-    {ok, Context} = erlzmq:context(),
-    {ok, Socket} = erlzmq:socket(Context, sub),
-    ok = erlzmq:setsockopt(Socket, subscribe, Identity),
-    ok = erlzmq:bind(Socket, Address),
-    {ok, Socket}.
-
-%% @doc convenience function to start the loop
-run(Socket, Pid) ->
-    loop(Socket, Pid).
 
 %% @doc run a receiver handler using the provided socket casting messages to
 %% the provided gen_server identifier gen server needs to handle
 %% cast {em2_reply, Message}
-loop(Socket, Pid) ->
+loop(Socket) ->
     {ok, Message} = erlzmq:recv(Socket),
-    gen_server:cast(Pid, {em2_reply, Message}),
-    loop(Socket, Pid).
+    error_logger:info_msg("~p ~n", [Message]),
+    loop(Socket).
